4ec73926f353fd42a16b574d6ea68691164ab782
ulyssecarion <ulyssecarion@gmail.com>
2013-03-10 14:25:47 -0700

    Initial rails commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sailing::Application.config.secret_token = '8ddba10ccf4493e6ef6aa851a24f7f42a56e110e3e02d13ac152f0e69c6521cc0cf78502b215718b702cb84409bf5040c9a653c7151a347e96f03ef18d514caf'

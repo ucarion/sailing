fafdcafd1e768dbe9f0b87862afde4aab64fb1b9
ulyssecarion <ulyssecarion@gmail.com>
2013-03-10 15:45:22 -0700

    Add about page.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Sailing::Application.routes.draw do
 
   match '/help', to: 'static_pages#help'
   match '/contact', to: 'static_pages#contact'
+  match '/about', to: 'static_pages#about'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.

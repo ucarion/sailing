6ba98a6242ea957c2454e22aa45e4ed1dd74f141
ulyssecarion <ulyssecarion@gmail.com>
2013-03-10 16:22:05 -0700

    Add basic authentication to users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,19 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
 
   before_save { |user| user.email = email.downcase }
 
   EMAIL_REGEX = /\A\w+@\w+\.[a-z]+\z/i
 
-  validates :name,  presence: true
+  validates :name, presence: true
+
   validates :email, presence: true,
                     format: { with: EMAIL_REGEX }
                     uniqueness: { case_sensitive: false }
+
+  validates :password, presence: true,
+                       length: { minimum: 6 }
+  
+  validates :password_confirmation, presence: true
 end

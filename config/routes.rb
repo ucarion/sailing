2fb9d9cbb2e5bb44570e17f86a51ceb6d43f272c
ulyssecarion <ulyssecarion@gmail.com>
2013-03-10 15:39:22 -0700

    Add help page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Sailing::Application.routes.draw do
   root to: 'static_pages#home'
 
+  match '/help', to: 'static_pages#help'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
